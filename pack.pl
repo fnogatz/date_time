12935178591ca8a5119d837e18a6236a4ab1476e
Falco Nogatz <fnogatz@gmail.com>
2019-01-30 18:26:54 +0100

    Add pack.pl

---
+++ pack.pl
@@ -0,0 +1,11 @@
+name(date_time).
+
+version('0.1.0').
+download('https://github.com/fnogatz/date_time/releases/*.zip').
+
+title('Logical arithmetic on dates and times').
+author('Falco Nogatz','fnogatz@gmail.com').
+maintainer('Falco Nogatz','fnogatz@gmail.com').
+packager('Falco Nogatz','fnogatz@gmail.com').
+
+home('https://github.com/fnogatz/date_time').
